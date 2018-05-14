0590edcd05f0e02e1bcd6f488b1bf9417ff0cde3
barakzan <barakzan@gmail.com>
2018-05-14 20:00:17 +0300

    switched to madrid

--- weatherForecast/dataPreparation.py
+++ weatherForecast/dataPreparation.py
@@ -3,16 +3,14 @@ import numpy as np
 
 
 def data_preparator():
-    important_features = ['ELEVATION', 'LATITUDE', 'LONGITUDE', 'DATE', 'DAILYMaximumDryBulbTemp',
-        'DAILYMinimumDryBulbTemp', 'DAILYAverageDryBulbTemp', 'DAILYDeptFromNormalAverageTemp',
-        'DAILYAverageRelativeHumidity', 'DAILYAverageDewPointTemp', 'DAILYAverageWetBulbTemp', 'DAILYHeatingDegreeDays',
-        'DAILYCoolingDegreeDays', 'DAILYSunrise', 'DAILYSunset', 'DAILYPrecip', 'DAILYSnowfall', 'DAILYSnowDepth',
-        'DAILYAverageStationPressure', 'DAILYAverageWindSpeed', 'DAILYPeakWindSpeed', 'PeakWindDirection',
-        'DAILYSustainedWindSpeed', 'DAILYSustainedWindDirection']
-    raw_data = pd.read_csv('../DataBases/rawDataBasebeforeBasicFiltering.csv', sep=',', low_memory=False)
-    raw_data = raw_data.dropna(subset=['DAILYSnowfall'], axis=0)
+    important_features = ['CET', 'Max TemperatureC', 'Mean TemperatureC', 'Min TemperatureC', 'Dew PointC',
+        'MeanDew PointC', 'Min DewpointC', 'Max Humidity', ' Mean Humidity', ' Min Humidity',
+        ' Max Sea Level PressurehPa', ' Mean Sea Level PressurehPa', ' Min Sea Level PressurehPa', ' Max VisibilityKm',
+        ' Mean VisibilityKm', ' Min VisibilitykM', ' Max Wind SpeedKm/h', ' Mean Wind SpeedKm/h', 'Precipitationmm',
+        ' CloudCover', 'WindDirDegrees']
+    raw_data = pd.read_csv('../madridDataBase/weather_madrid_LEMD_1997_2015.csv', sep=',', low_memory=False)
     raw_data = raw_data[important_features]
-    raw_data.to_csv(path_or_buf='../DataBases/FilteredrawDate.csv', index=False)
+    raw_data.to_csv(path_or_buf='../madridDataBase/FilteredRawDate.csv', index=False)
 
 
 if __name__ == "__main__":
