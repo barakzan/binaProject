083501f86502cab0579d7fa42481d660617666a6
barakzan <barakzan@gmail.com>
2018-06-06 18:55:03 +0300

    work together

--- weatherForecast/AIWeatherForecast.py
+++ weatherForecast/AIWeatherForecast.py
@@ -1,40 +1,77 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
-import datetime
 import dataPreparation
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 import logging
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+make_new_database = False
 
 
-def main():
-    # TODO: take last ver by default
-    version = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
-    dataPreparation.prepare_data(version)
+def calculate_precision(df, x, feature):
+    """calculate the precision of the algorithm up to x degrees"""
+    logging.info("calculate_precision")
+
+    for i in range(x):
+        df["precision_up_to_" + str(i) + "_degrees"] = np.where(df[feature] <= i, 1, 0)
+
+
+def forecast():
+    if make_new_database:
+        version = dataPreparation.prepare_data(fill_missing_from_previous_day=False,
+                                               days_before=7, trending_before=14, average_before=14)
+        file = open("last_version.txt", 'w')
+        file.write(version)
+    else:
+        file = open("last_version.txt", 'r')
+        version = file.read()
     df = pd.read_csv('..\\madridDataBase\\prepared_data_' + version + '.csv', sep=',', header=0)
 
     # split to train, validation and test sets
-    # TODO: change test size to be a parameter
     train_and_validation, test = train_test_split(df, test_size=0.2, shuffle=False)
     train, validation = train_test_split(train_and_validation, test_size=0.2, shuffle=False)
     
-    # train knn algorithm
-    # TODO: make K a parameter
-    knn_classifier = KNeighborsClassifier(n_neighbors=3)
-    knn_classifier.fit(train.drop(['Mean TemperatureC'], axis=1), train['Mean TemperatureC'])
-    test_predictions = knn_classifier.predict(test.drop(['Mean TemperatureC'], axis=1))
+    # train the decision tree classifier
+    classifier = DecisionTreeClassifier()
+    classifier.fit(train.drop(['Mean TemperatureC'], axis=1), train['Mean TemperatureC'])
+    test_predictions = classifier.predict(test.drop(['Mean TemperatureC'], axis=1))
 
-    print('accuracy:', metrics.accuracy_score(test['Mean TemperatureC'], test_predictions) * 100, '%')
-    # print('precision:', metrics.precision_score(test['Mean TemperatureC'], test_predictions))
-    # print('recall:', metrics.recall_score(test['Mean TemperatureC'], test_predictions))
-    # print('f1 score:', metrics.f1_score(test['Mean TemperatureC'], test_predictions))
+    results_df = pd.DataFrame({'real_value': test['Mean TemperatureC'], 'test_predictions': test_predictions})
+    results_df['abs_diff'] = abs(results_df['real_value'] - results_df['test_predictions'])
+    calculate_precision(results_df, 4, 'abs_diff')
+    total = results_df["precision_up_to_1_degrees"].count()
+    accurate = results_df["precision_up_to_0_degrees"].sum()
+    one_degree = results_df["precision_up_to_1_degrees"].sum()
+    two_degrees = results_df["precision_up_to_2_degrees"].sum()
+    three_degrees = results_df["precision_up_to_3_degrees"].sum()
+    accurate = (accurate/total)*100
+    one_degree = (one_degree/total)*100
+    two_degrees = (two_degrees/total)*100
+    three_degrees = (three_degrees/total)*100
+    parameter_to_improve = (3*accurate + 2*one_degree + two_degrees) / 6
+
+    print('******************')
+    print('******************')
+
+    print("zero degree accuracy = ", round(accurate, 2))
+    print("one degrees accuracy = ", round(one_degree, 2))
+    print("two degrees accuracy = ", round(two_degrees, 2))
+    print("three degrees accuracy = ", round(three_degrees, 2))
+    print("- - - - - - - - - - - - - - - - - - - ")
+    print("parameter_to_improve = ", round(parameter_to_improve, 3))
+
+    # sprint(results_df['abs_diff'].value_counts())
+    print('******************')
+    print('******************')
+
     
     logging.info("END TIME")
     # print("features_to_drop:\n", features_to_drop)
     
 
 if __name__ == '__main__':
-    main()
+    forecast()
--- weatherForecast/dataPreparation.py
+++ weatherForecast/dataPreparation.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import logging
+import datetime
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 
@@ -12,6 +13,7 @@ toPredict = 'Mean TemperatureC'
 
 def remove_low_data_features(df, threshold=2 / 3):
     """remove every feature that has at list threshold the data missing"""
+    logging.info("remove_low_data_features")
 
     num_of_days = df.shape[0]
     df.dropna(thresh=(num_of_days * threshold), axis=1, inplace=True)
@@ -19,16 +21,24 @@ def remove_low_data_features(df, threshold=2 / 3):
 
 def _sample_dist(sample1, sample2):
     """return the distance between to samples"""
+
     return (sample1.subtract(sample2).abs()).sum()
+
+
+def fill_from_previous_day(df):
+    """fill the nan in data set using closest fit"""
+    logging.info("fill_from_previous_day")
+
+    df.fillna(method='ffill', inplace=True)
 
 
 def closest_fit(df, trainDf, numOfSamples=100, useLabel=False):
     """fill the nan in data set using closest fit"""
-    logging.info("***start closest_fit")
+    logging.info("closest_fit")
 
     for idx, sample in df.iterrows():
         if sample.isnull().any():
-            null_columns = df.columns[sample.isnull() == True].tolist()
+            null_columns = df.columns[sample.isnull()].tolist()
             if not useLabel:
                 null_columns.append(labelName)
 
@@ -54,59 +64,94 @@ def _derive_nth_day_feature(df, feature, N):
 
     rows = df.shape[0]
     nth_prior_measurements = [None] * N + [df[feature][i - N] for i in range(N, rows)]
-    col_name = "{}_{}".format(feature, N)
+    col_name = "{}_{}_days_before".format(feature, N)
     df[col_name] = nth_prior_measurements
 
 
-def sign_sqrt(x):
+def _sign_sqrt(x):
+    """calculate sign sqrt"""
+
     return np.sign(x)*np.sqrt(np.abs(x))
 
 
-def sign_square(x):
+def _sign_square(x):
+    """calculate sign sqrt"""
+
     return np.sign(x)*np.square(x)
 
 
-def features_trend(df, features, x):
-    for feature in features:
-        df[feature+"trend_in_last" + str(x) +"_days"] = df[feature].diff()\
-                .apply(sign_sqrt).rolling(window=x).mean().apply(sign_square)
+def features_last_x_days_average(df, features, x):
+    """calculate the average of last x days for feature in features"""
+    logging.info("features_last_" +str(x) + "_days_average")
 
-def features_deriving(df, features, N):
+    for feature in features:
+        df[feature+"average_of_last_" + str(x) +"_days"] = df[feature].rolling(window=x).mean()
+
+
+def features_trend_type1(df, features, x):
+    """calculate the trend of last x days for feature in features"""
+    logging.info("features_trend_type1 of last " + str(x) + " days")
+
+    for feature in features:
+        df[feature+"trend_in_last_" + str(x) +"_days"] = df[feature].diff()\
+                .apply(_sign_sqrt).rolling(window=x).mean().apply(_sign_square)
+
+
+def features_trend_type2(df, features, x):
+    """calculate the trend of last x days for feature in features"""
+    logging.info("features_trend_type2 of last " + str(x) + " days")
+
+    for feature in features:
+        df[feature + "trend_in_last_" + str(x) + "_days"] = df[feature].diff() \
+            .apply(_sign_square).rolling(window=x).mean().apply(_sign_sqrt)
+
+
+def features_deriving(df, features, x):
     """derive all features in features list for all the previus N days"""
-    logging.info("***start features_deriving")
+    logging.info("start features_deriving")
 
     for feature in features:
         if feature != 'date':
-            for n in range(1, N + 1):
+            for n in range(1, x + 1):
                 _derive_nth_day_feature(df, feature, n)
 
 
-def prepare_data(output_file_version=''):
+def prepare_data(days_before=3, trending_before=7, average_before=7,
+                 fill_missing_from_previous_day=True):
+
     logging.info("START TIME")
 
     """Load the weather history"""
     df = pd.read_csv(raw_data_file, sep=',', header=0)
 
+    # remove features that dose not have enough days with data on them
     remove_low_data_features(df)
     features = df.columns.tolist()
 
     # Imputation - fill missing values
-    closest_fit(df, df, 10)
+    if fill_missing_from_previous_day is True:
+        fill_from_previous_day(df)
+    else:
+        closest_fit(df, df, 10)
 
     # create feature for each date based on N previous days
-    features_to_derive = ['Mean TemperatureC', 'MeanDew PointC', ' Mean Humidity', ' Min Humidity',
-                          ' Mean Sea Level PressurehPa', ' Mean VisibilityKm', ' Mean Wind SpeedKm/h',
-                          'Precipitationmm', ' CloudCover', 'WindDirDegrees']
+    features_to_derive = list(features)
+    features_to_derive.remove('CET')
+    features_deriving(df, features_to_derive, days_before)
 
-    features_deriving(df, features_to_derive, 5)
+    # create feature of the average for each date based on x previous days
+    features_to_calculate_average = features_to_derive
+    if average_before > 2:
+        for i in range(2, average_before):
+            features_last_x_days_average(df, features_to_calculate_average, i)
 
-    # TODO: most of the work is here
+    # create feature of the trend for each date based on 3 and 6 previous days
+    features_to_calculate_trend = features_to_derive
 
-    # create feature for each date based on 3 and 6 previous days
-    # TODO: add features_to_calculate
-    features_to_calculate_trend = ['Mean TemperatureC', 'MeanDew PointC']
-    features_trend(df, features_to_calculate_trend, 3)
-    features_trend(df, features_to_calculate_trend, 6)
+    if trending_before >= 2:
+        for i in range(2, trending_before + 1):
+            features_trend_type1(df, features_to_calculate_trend, i)
+            features_trend_type2(df, features_to_calculate_trend, i)
 
     # drop dates with missing data
     df.dropna(how='any', inplace=True)
@@ -117,7 +162,11 @@ def prepare_data(output_file_version=''):
     
     # TODO: remove redundant feature using pearson correlation and mutual information
 
-    df.to_csv('..\\madridDataBase\\prepared_data_' + output_file_version + '.csv')
+    version = "{}__days_before={}_trending_before={}_average_before={}_fill_missing_from_previous_day={}"\
+        .format(datetime.datetime.now().strftime("%Y_%m_%d__%H-%M"), days_before, trending_before, average_before,
+                fill_missing_from_previous_day)
+    df.to_csv('..\\madridDataBase\\prepared_data_' + version + '.csv')
+    return version
 
 
 if __name__ == "__main__":
