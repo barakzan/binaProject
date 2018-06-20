4601680c16248b9f75cc9b85046dc7712f8e8996
barakzan <barakzan@gmail.com>
2018-06-20 17:58:53 +0300

    added empty exp

---
+++ weatherForecast/experiments.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import logging
+import datetime
+
+
+def experiment():
+    for i in range(1, 15):
+        i = i+1
+
+
+if __name__ == "__main__":
+    experiment()
+
