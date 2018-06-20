e70fedbdbe542008d9709600017db69986d6bfc2
barakzan <barakzan@gmail.com>
2018-06-20 18:15:16 +0300

    .

--- weatherForecast/experiments.py
+++ weatherForecast/experiments.py
@@ -3,11 +3,13 @@ import numpy as np
 import pandas as pd
 import logging
 import datetime
+import AIWeatherForecast
 
 
 def experiment():
+    results = []
     for i in range(1, 15):
-        i = i+1
+        results[i] = AIWeatherForecast.forecast(i)
 
 
 if __name__ == "__main__":
