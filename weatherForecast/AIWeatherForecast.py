b63bb3c5705fd2ff841d6053bbcbc241693b2736
barakzan <barakzan@gmail.com>
2018-10-20 12:26:41 +0300

    save so ofir wont delete everything

--- weatherForecast/AIWeatherForecast.py
+++ weatherForecast/AIWeatherForecast.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 import logging
@@ -23,7 +24,7 @@ def calculate_precision(df, x, feature):
 
 
 def calculate_results(results_df, print_results=False):
-    results_df['abs_diff'] = abs(results_df['real_value'] - results_df['test_predictions'])
+    results_df['abs_diff'] = abs(round(results_df['real_value'] + 0.5) - results_df['validation_predictions'])
     calculate_precision(results_df, 4, 'abs_diff')
     total = results_df["precision_up_to_1_degrees"].count()
     accurate = results_df["precision_up_to_0_degrees"].sum()
@@ -34,22 +35,17 @@ def calculate_results(results_df, print_results=False):
     one_degree = (one_degree/total)*100
     two_degrees = (two_degrees/total)*100
     three_degrees = (three_degrees/total)*100
-    parameter_to_improve = (3*accurate + 2*one_degree + two_degrees) / 6
 
     if print_results:
-        print('******************')
-        print("zero degree accuracy = ", round(accurate, 2))
-        print("one degrees accuracy = ", round(one_degree, 2))
-        print("two degrees accuracy = ", round(two_degrees, 2))
-        print("three degrees accuracy = ", round(three_degrees, 2))
-        print("- - - - - - - - - - - - - - - - - - - ")
-        print("parameter_to_improve = ", round(parameter_to_improve, 3))
-        print('******************')
+        print("\tzero degree accuracy = ", round(accurate, 2))
+        print("\tone degrees accuracy = ", round(one_degree, 2))
+        print("\ttwo degrees accuracy = ", round(two_degrees, 2))
+        print("\tthree degrees accuracy = ", round(three_degrees, 2))
 
     return [accurate, one_degree, two_degrees, three_degrees]
 
 
-def forecast(days_before=7, use_forest_instead_of_tree=True, fill_missing_from_previous_day=True):
+def forecast(days_before=7, classifier_type='forest', fill_missing_from_previous_day=True, max_depth=10):
     trending_before = days_before
     average_before = days_before
 
@@ -75,43 +71,124 @@ def forecast(days_before=7, use_forest_instead_of_tree=True, fill_missing_from_p
     # makes a new joined data frame
     joined_df = pd.concat([austin_df, train], ignore_index=True)
 
-    # choose tree or forest classifier
-    if use_forest_instead_of_tree:
-        classifier = RandomForestClassifier(n_estimators=22, bootstrap=False)
-        joined_classifier = RandomForestClassifier(n_estimators=22, bootstrap=False)
-    else:
+    # choose tree, forest or linear classifier
+    if classifier_type == 'forest':
+        classifier = RandomForestClassifier(n_estimators=22, bootstrap=False, max_depth=max_depth)
+        joined_classifier = RandomForestClassifier(n_estimators=22, bootstrap=False, max_depth=max_depth)
+    elif classifier_type == 'tree':
         # train the decision tree classifier
         classifier = DecisionTreeClassifier()
         joined_classifier = DecisionTreeClassifier()
+    else:
+        classifier = LinearRegression()
+        joined_classifier = LinearRegression()
 
     # train madrid classifier
     classifier.fit(train.drop(['Mean TemperatureC'], axis=1), train['Mean TemperatureC'])
 
     # predict
-    madrid_test_predictions = classifier.predict(test.drop(['Mean TemperatureC'], axis=1))
-    austin_test_predictions = classifier.predict(austin_df.drop(['Mean TemperatureC'], axis=1))
+    madrid_validation_predictions = classifier.predict(validation.drop(['Mean TemperatureC'], axis=1))
+    austin_validation_predictions = classifier.predict(austin_df.drop(['Mean TemperatureC'], axis=1))
 
     # train joined classifier
     joined_classifier.fit(joined_df.drop(['Mean TemperatureC'], axis=1), joined_df['Mean TemperatureC'])
 
     # predict
-    madrid_joined_test_predictions = joined_classifier.predict(test.drop(['Mean TemperatureC'], axis=1))
+    madrid_joined_validation_predictions = joined_classifier.predict(validation.drop(['Mean TemperatureC'], axis=1))
 
     # get results
-    madrid_results_df = pd.DataFrame({'real_value': test['Mean TemperatureC'],
-                                      'test_predictions': madrid_test_predictions})
+    madrid_results_df = pd.DataFrame({'real_value': validation['Mean TemperatureC'],
+                                      'validation_predictions': madrid_validation_predictions})
     austin_results_df = pd.DataFrame({'real_value': austin_df['Mean TemperatureC'],
-                                      'test_predictions': austin_test_predictions})
-    madrid_joined_results_df = pd.DataFrame({'real_value': test['Mean TemperatureC'],
-                                             'test_predictions': madrid_joined_test_predictions})
-    #calculate precision
+                                      'validation_predictions': austin_validation_predictions})
+    madrid_joined_results_df = pd.DataFrame({'real_value': validation['Mean TemperatureC'],
+                                             'validation_predictions': madrid_joined_validation_predictions})
+    # calculate precision
     madrid_precision = calculate_results(madrid_results_df, print_results=False)
     austin_precision = calculate_results(austin_results_df, print_results=False)
     madrid_joined_precision = calculate_results(madrid_joined_results_df, print_results=False)
 
-    logging.info("END forecast: " + str(days_before) + " days before, forest: " + str(use_forest_instead_of_tree))
+    logging.info("END forecast: " + str(days_before) + " days before, forest: " + str(classifier_type))
 
     return [madrid_precision, austin_precision, madrid_joined_precision]
+
+
+def final_project_results(days_before=7, classifier_type='tree', fill_missing_from_previous_day=True, max_depth=10):
+    trending_before = days_before
+    average_before = days_before
+
+    madrid_file_name = dataPreparation.get_file_name('madrid', days_before, fill_missing_from_previous_day)
+    madrid_file = Path(madrid_file_name)
+    if not madrid_file.is_file():
+        dataPreparation.prepare_data('madrid', days_before, trending_before, average_before,
+                                     fill_missing_from_previous_day)
+
+    austin_file_name = dataPreparation.get_file_name('austin', days_before, fill_missing_from_previous_day)
+    austin_file = Path(austin_file_name)
+    if not austin_file.is_file():
+        dataPreparation.prepare_data('austin', days_before, trending_before, average_before,
+                                     fill_missing_from_previous_day)
+
+    madrid_df = pd.read_csv(madrid_file_name, sep=',', header=0)
+    austin_df = pd.read_csv(austin_file_name, sep=',', header=0)
+
+    train, test = train_test_split(madrid_df, test_size=0.2, shuffle=False)
+
+    joined_df = pd.concat([austin_df, train], ignore_index=True)
+
+    # choose tree, forest or linear classifier
+    if classifier_type == 'forest':
+        classifier = RandomForestClassifier(n_estimators=22, bootstrap=False, max_depth=max_depth)
+        joined_classifier = RandomForestClassifier(n_estimators=22, bootstrap=False, max_depth=max_depth)
+    elif classifier_type == 'tree':
+        # train the decision tree classifier
+        classifier = DecisionTreeClassifier()
+        joined_classifier = DecisionTreeClassifier()
+    else:
+        classifier = LinearRegression()
+        joined_classifier = LinearRegression()
+
+
+    # train madrid classifier
+    classifier.fit(train.drop(['Mean TemperatureC'], axis=1), train['Mean TemperatureC'])
+
+    # train joined classifier
+    joined_classifier.fit(joined_df.drop(['Mean TemperatureC'], axis=1), joined_df['Mean TemperatureC'])
+
+    # predict
+    madrid_test_predictions = classifier.predict(test.drop(['Mean TemperatureC'], axis=1))
+    austin_test_predictions = classifier.predict(austin_df.drop(['Mean TemperatureC'], axis=1))
+    madrid_joined_test_predictions = joined_classifier.predict(test.drop(['Mean TemperatureC'], axis=1))
+
+    # get results
+    madrid_results_df = pd.DataFrame({'real_value': test['Mean TemperatureC'],
+                                      'validation_predictions': madrid_test_predictions})
+    austin_results_df = pd.DataFrame({'real_value': austin_df['Mean TemperatureC'],
+                                      'validation_predictions': austin_test_predictions})
+    madrid_joined_results_df = pd.DataFrame({'real_value': test['Mean TemperatureC'],
+                                             'validation_predictions': madrid_joined_test_predictions})
+    # calculate precision
+    print("\nmadrid precision:")
+    calculate_results(madrid_results_df, print_results=True)
+    print("\naustin precision:")
+    calculate_results(austin_results_df, print_results=True)
+    print("\nmadrid joined precision:")
+    calculate_results(madrid_joined_results_df, print_results=True)
+    print("\nyesterday precision:")
+    predict_yesterday(print_results=True)
+
+
+def predict_yesterday(print_results):
+    madrid_file_name = dataPreparation.get_file_name('madrid', 1, fill_missing_from_previous_day=True)
+    madrid_file = Path(madrid_file_name)
+    if not madrid_file.is_file():
+        dataPreparation.prepare_data('madrid', 1, 1, 1, fill_missing_from_previous_day=True)
+
+    madrid_df = pd.read_csv(madrid_file_name, sep=',', header=0)
+
+    madrid_yesterday_results_df = pd.DataFrame({'real_value': madrid_df['Mean TemperatureC'],
+                                                'validation_predictions': madrid_df['Mean TemperatureC'].shift(1)})
+    return calculate_results(madrid_yesterday_results_df, print_results)
 
 
 if __name__ == '__main__':
--- weatherForecast/experiments.py
+++ weatherForecast/experiments.py
@@ -8,25 +8,34 @@ import AIWeatherForecast
 import dataPreparation
 
 
-def experiment(max_days=14):
-    df = pd.DataFrame(np.zeros([24, max_days]), columns=[str(i) for i in range(1, max_days + 1)],
+def experiment(max_days=14, max_depth=10):
+    df = pd.DataFrame(np.zeros([36, max_days]), columns=[str(i) for i in range(1, max_days + 1)],
                       index=['madrid forest 0', 'austin forest 0', 'joined forest 0',
                              'madrid tree 0', 'austin tree 0', 'joined tree 0',
+                             'madrid linear 0', 'austin linear 0', 'joined linear 0',
                              'madrid forest 1', 'austin forest 1', 'joined forest 1',
                              'madrid tree 1', 'austin tree 1', 'joined tree 1',
+                             'madrid linear 1', 'austin linear 1', 'joined linear 1',
                              'madrid forest 2', 'austin forest 2', 'joined forest 2',
                              'madrid tree 2', 'austin tree 2', 'joined tree 2',
+                             'madrid linear 2', 'austin linear 2', 'joined linear 2',
                              'madrid forest 3', 'austin forest 3', 'joined forest 3',
-                             'madrid tree 3', 'austin tree 3', 'joined tree 3'])
+                             'madrid tree 3', 'austin tree 3', 'joined tree 3',
+                             'madrid linear 3', 'austin linear 3', 'joined linear 3'])
     for i in range(1, max_days + 1):
-        forest_res = AIWeatherForecast.forecast(i, use_forest_instead_of_tree=True)
+        forest_res = AIWeatherForecast.forecast(i, classifier_type='forest', max_depth=max_depth)
         madrid_forest_res = forest_res[0]
         austin_forest_res = forest_res[1]
         joined_forest_res = forest_res[2]
-        tree_res = AIWeatherForecast.forecast(i, use_forest_instead_of_tree=False)
+        tree_res = AIWeatherForecast.forecast(i, classifier_type='tree', max_depth=max_depth)
         madrid_tree_res = tree_res[0]
         austin_tree_res = tree_res[1]
         joined_tree_res = tree_res[2]
+        linear_res = AIWeatherForecast.forecast(i, classifier_type='linear', max_depth=max_depth)
+        madrid_linear_res = linear_res[0]
+        austin_linear_res = linear_res[1]
+        joined_linear_res = linear_res[2]
+
         for j in range(0, 4):
             df.loc['madrid forest ' + str(j), str(i)] = round(madrid_forest_res[j], 2)
             df.loc['austin forest ' + str(j), str(i)] = round(austin_forest_res[j], 2)
@@ -34,8 +43,14 @@ def experiment(max_days=14):
             df.loc['madrid tree ' + str(j), str(i)] = round(madrid_tree_res[j], 2)
             df.loc['austin tree ' + str(j), str(i)] = round(austin_tree_res[j], 2)
             df.loc['joined tree ' + str(j), str(i)] = round(joined_tree_res[j], 2)
+            df.loc['madrid linear ' + str(j), str(i)] = round(madrid_linear_res[j], 2)
+            df.loc['austin linear ' + str(j), str(i)] = round(austin_linear_res[j], 2)
+            df.loc['joined linear ' + str(j), str(i)] = round(joined_linear_res[j], 2)
 
     df.to_csv("results.csv")
+
+    AIWeatherForecast.final_project_results(days_before=3, classifier_type='tree', fill_missing_from_previous_day=True,
+                                            max_depth=10)
 
 
 def plot_results():
@@ -63,13 +78,24 @@ def plot_results():
     plt.figure(2)
     temp_df['tree 0' + degree_sign + ' accuracy'] = df['madrid tree 0']
     temp_df['forest 0' + degree_sign + ' accuracy'] = df['madrid forest 0']
+    temp_df['linear 0' + degree_sign + ' accuracy'] = df['madrid linear 0']
+
+    temp_df['linear 1' + degree_sign + ' accuracy'] = df['madrid linear 1']
+
     temp_df['tree 2' + degree_sign + ' accuracy'] = df['madrid tree 2']
     temp_df['forest 2' + degree_sign + ' accuracy'] = df['madrid forest 2']
+    temp_df['linear 2' + degree_sign + ' accuracy'] = df['madrid linear 2']
     temp_df['tree 0' + degree_sign + ' accuracy'].plot(style='r^-')
     temp_df['forest 0' + degree_sign + ' accuracy'].plot(style='g^-')
+    temp_df['linear 0' + degree_sign + ' accuracy'].plot(style='b^-')
+
+    temp_df['linear 1' + degree_sign + ' accuracy'].plot(style='b+')
+
     temp_df['tree 2' + degree_sign + ' accuracy'].plot(style='r')
     temp_df['forest 2' + degree_sign + ' accuracy'].plot(style='g')
-    plt.title('Madrid accuracy comparisson tree clf vs forest clf')
+    temp_df['linear 2' + degree_sign + ' accuracy'].plot(style='b')
+
+    plt.title('Madrid accuracy comparision tree clf vs forest clf vs linear clf')
     plt.xlabel('Days')
     plt.xticks(range(14), range(1, 15))
     plt.ylabel('Accuracy[%]')
@@ -154,7 +180,6 @@ def plot_results():
     plt.xlabel('temperature[C'+ degree_sign + ']')
 
     plt.show()
-
 
 if __name__ == "__main__":
     experiment()
